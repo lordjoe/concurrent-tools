34d6997b0babdf116722e2cfd351c0a22f90f64c
lordjoe2000 <lordjoe2000@gmail.com>
2014-09-10 17:49:57 +0000

    Updated MapReductRunner

--- src/main/java/uk/ac/ebi/pride/hadooprunner/IMapReduceRunner.java
+++ src/main/java/uk/ac/ebi/pride/hadooprunner/IMapReduceRunner.java
@@ -1,12 +1,47 @@
 package uk.ac.ebi.pride.hadooprunner;
 
+import java.util.*;
+
 /**
  * uk.ac.ebi.pride.hadooprunner.IMapReduceRunner
  *
  * @author Steve Lewis
  * @date 5/14/14
  */
-public interface IMapReduceRunner {
-    public static IMapReduceRunner[] EMPTY_ARRAY = {};
-    public static Class THIS_CLASS = IMapReduceRunner.class;
+public interface IMapReduceRunner<K,D> {
+    /**
+     * equivalent of write in hadoop
+      * @param key
+     * @param value
+     */
+    public void saveKeyValue(K key, D value);
+
+    /**
+     * main reduce step
+     * @param key
+     * @param values
+     */
+    public void mapKeyValues(K key,D value);
+
+    /**
+     * main reduce step
+     * @param key
+     * @param values
+     */
+    public void handleKeyValues(K key,Iterator<D> values);
+
+    /**
+     * any operations to setup
+     * @param data - any data wo be passed in
+     *             implementation specific
+     */
+    public void setupMapReduce(Object... data);
+
+    /**
+     * any operations to close
+     * @param data - any data wo be passed in
+     */
+    public void shutdownMapReduce(Object... data);
+
+
 }
